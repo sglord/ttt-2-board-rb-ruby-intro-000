1584bf0d126271f1f5f8fff3c790beb01117b836
S L <lordstepheng@gmail.com>
2018-08-26 23:57:01 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,39 @@
 # Define the variable board below.
 
+<<<<<<< HEAD
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+=======
+
+
+# Position 1: TopLeft
+row[0]
+
+# Position 2: TopMiddle
+row[1]
+
+# Position 3: TopRight
+row[2]
+
+# Position 4: MidLeft
+row[3]
+
+# Position 5: MidMiddle
+row[4]
+
+# Position 6: MidRight
+row[5]
+
+# Position 7: BotLeft
+row[6]
+
+# Position 8: BotMiddle
+row[7]
+
+# Position 9: BotRight
+row[8]
+
+
+# Move X to the Position 2, Middle
+row[1] = "X"
+row #=> [" ", "X", " "]
+>>>>>>> 396de4cabf4471a0b3f9df35596625fa2b41b7df
